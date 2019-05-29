28b00be04a2a12740918548c6501967bb0f8e57f
Bryan Giger <bryan@gigerbyt.es>
2019-05-29 10:29:24 -0400

    change title

--- index.js
+++ index.js
@@ -73,6 +73,28 @@ app.post('/slack/approve', (req, res) => {
   }
 })
 
+// charge
+app.post("/charge", (req, res) => {
+  let amount = 5000;
+
+  stripe.customers.create({
+    email: req.body.email,
+    card: req.body.id
+  })
+  .then(customer =>
+    stripe.charges.create({
+      amount,
+      description: "Digital Paraguay - Job Post",
+      currency: "usd",
+      customer: customer.id
+    }))
+  .then(charge => res.send(charge))
+  .catch(err => {
+    console.log("Error:", err);
+    res.status(500).send({error: "Purchase Failed"});
+  });
+});
+
 MongoClient.connect(
   MongoUrl,
   { useNewUrlParser: true },
